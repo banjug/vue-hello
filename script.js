5732df896327224e0ad533dc6b16cc8b18f40cc6
Samuele <miotto.samuele@gmail.com>
2021-11-10 14:20:32 +0100

    add image generation and styling on click

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 var app = new Vue({
         el: '#root',
         data: {
-            message: 'Hello World',
+            message: "Click me",
+            imgSrc: "img/img.jpg",
+            miaClasse: ''
+        },
+        methods: {
+            clickBlur: function() {
+                if (this.miaClasse == '') {
+                    this.miaClasse = 'blur';
+                } else if (this.miaClasse = 'blur') {
+                    this.miaClasse = '';
+                }
+            }
         }
     }
 );
